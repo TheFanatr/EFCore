16c4f84ddbb518838227b86dc97e6dfdd967735a
Alexander Fanat <alex.fanat@smus.ca>
2020-08-20 08:12:42 -0700

    Remove additional accidental artifacts, extract GeneralStructuralComparer class out of ArrayStructuralComparer then delete ArrayStructuralComparer.cs file.

--- src/EFCore/ChangeTracking/GeneralStructuralComparer.cs
+++ src/EFCore/ChangeTracking/GeneralStructuralComparer.cs
@@ -1,35 +1,11 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace Microsoft.EntityFrameworkCore.ChangeTracking
 {
-    /// <summary>
-    ///     <para>
-    ///         Specifies value snapshotting and comparison for arrays where each element is compared
-    ///         a new array is constructed when snapshotting.
-    ///     </para>
-    /// </summary>
-    /// <typeparam name="TElement"> The array element type. </typeparam>
-    public class ArrayStructuralComparer<TElement> : ValueComparer<TElement[]>
-    {
-        /// <summary>
-        ///     Creates a comparer instance.
-        /// </summary>
-        public ArrayStructuralComparer()
-            : base(
-                CreateDefaultEqualsExpression(),
-                CreateDefaultHashCodeExpression(favorStructuralComparisons: true),
-                v => v == null ? null : v.ToArray())
-        {
-        }
-    }
-
     /// <summary>
     ///     <para>
     ///         Specifies value snapshotting and comparison for arrays where each element is compared
